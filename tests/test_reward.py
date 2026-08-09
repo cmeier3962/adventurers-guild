026e22df38015b7dd3a2ef22c2741495e50e3184
Chris Meier <christopher.meier3962@outlook.com>
2026-08-09 16:05:21 -0500

    created reward class and tests

---
+++ tests/test_reward.py
@@ -0,0 +1,69 @@
+import pytest
+
+from eryndor.enums import ItemType, ItemRarity
+from eryndor.item import Item
+from eryndor.reward import Reward
+
+
+### ---------- Fixtures ---------- ###
+@pytest.fixture
+def item() -> Item:
+    """Returns a valid item."""
+    return Item(
+        "item-001",
+        "Novice Sword",
+        "A basic sword.",
+        ItemType.WEAPON,
+        ItemRarity.COMMON,
+        10,
+    )
+
+
+@pytest.fixture
+def reward(item: Item) -> Reward:
+    """Returns a valid reward"""
+    return Reward(100, [item])
+
+
+### ---------- Initialization Tests ---------- ###
+def test_reward_initialization_default() -> None:
+    """Tests the creation of a default reward object."""
+    reward = Reward()
+    assert reward.experience == 0
+    assert reward.items == []
+
+
+def test_reward_initialization_custom_exp_only() -> None:
+    """Tests the creation of a custom reward object that only contains an experience reward."""
+    reward = Reward(experience=100)
+    assert reward.experience == 100
+    assert reward.items == []
+
+
+def test_reward_initialization_custom_items_only(item: Item) -> None:
+    """Tests the creation of a custom reward object that only contains an item(s) reward."""
+    reward = Reward(items=[item])
+    assert reward.experience == 0
+    assert item in reward.items
+
+
+def test_reward_initialization_custom_exp_and_items(item: Item, reward: Reward) -> None:
+    """Tests the creation of a custom reward object."""
+    assert reward.experience == 100
+    assert item in reward.items
+
+
+def test_reward_initialization_negative_experience() -> None:
+    """Tests a reward object's experience cannot be negative."""
+    with pytest.raises(ValueError, match="Experience reward must be >= 0"):
+        Reward(-100)
+
+
+def test_reward_items_are_independent_from_original_list(item: Item) -> None:
+    """Tests that reward stores its own copy of the item list."""
+    items = [item]
+    reward = Reward(items=items)
+
+    items.clear()
+
+    assert item in reward.items
